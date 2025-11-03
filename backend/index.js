e7fe3fb3698d5a6a453e358748da19dc24ee419b
RutuvikGenisys <rutuvik.p@genisys-group.com>
2025-11-03 17:52:18 +0530

    Add backend/index.js

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const deviceRoutes = require('./routes/deviceRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // For parsing application/json
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/devices', deviceRoutes);
+app.use('/api/orders', orderRoutes);
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
